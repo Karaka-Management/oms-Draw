a0dd5b4acab724f5dca4ac3b2b225e71614d2616
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-09-19 21:34:04 +0200

    Split controllers per application

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -12,7 +12,7 @@
  */
 declare(strict_types=1);
 
-namespace Modules\Draw;
+namespace Modules\Draw\Controller;
 
 use Model\Message\FormValidation;
 use Modules\Draw\Models\DrawImage;
@@ -40,57 +40,8 @@ use phpOMS\Message\Http\RequestStatusCode;
  * @link       http://website.orange-management.de
  * @since      1.0.0
  */
-final class Controller extends ModuleAbstract implements WebInterface
+class BackendController extends Controller
 {
-
-    /**
-     * Module path.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_PATH = __DIR__;
-
-    /**
-     * Module version.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_VERSION = '1.0.0';
-
-    /**
-     * Module name.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_NAME = 'Draw';
-
-    /**
-     * Module id.
-     *
-     * @var int
-     * @since 1.0.0
-     */
-    public const MODULE_ID = 1005200000;
-
-    /**
-     * Providing.
-     *
-     * @var string[]
-     * @since 1.0.0
-     */
-    protected static $providing = [];
-
-    /**
-     * Dependencies.
-     *
-     * @var string[]
-     * @since 1.0.0
-     */
-    protected static $dependencies = [
-    ];
 
     /**
      * @param RequestAbstract  $request  Request
@@ -186,97 +137,5 @@ final class Controller extends ModuleAbstract implements WebInterface
         $view->addData('images', $images);
 
         return $view;
-    }
-
-    /**
-     * Validate draw create request
-     *
-     * @param RequestAbstract $request Request
-     *
-     * @return array<string, bool>
-     *
-     * @since  1.0.0
-     */
-    private function validateDrawCreate(RequestAbstract $request) : array
-    {
-        $val = [];
-        if (($val['title'] = empty($request->getData('title')))
-            || ($val['image'] = empty($request->getData('image')))
-        ) {
-            return $val;
-        }
-
-        return [];
-    }
-
-    /**
-     * @param RequestAbstract  $request  Request
-     * @param ResponseAbstract $response Response
-     * @param mixed            $data     Generic data
-     *
-     * @return void
-     *
-     * @api
-     *
-     * @since  1.0.0
-     */
-    public function apiDrawCreate(RequestAbstract $request, ResponseAbstract $response, $data = null) : void
-    {
-        if (!empty($val = $this->validateDrawCreate($request))) {
-            $response->set('draw_create', new FormValidation($val));
-
-            return;
-        }
-
-        $path      = MediaController::createMediaPath();
-        $extension = 'png';
-        $filename  = '';
-        $rnd       = '';
-
-        // todo: implement limit since this could get exploited
-        do {
-            $filename  = sha1(((string) $request->getData('image')) . $rnd);
-            $filename .= '.' . $extension;
-
-            $rnd = mt_rand();
-        } while (file_exists($path . '/' . $filename));
-
-        $fullPath = __DIR__ . '/../../' . $path . '/' . $filename;
-
-        $this->createLocalFile($fullPath, (string) $request->getData('image'));
-
-        $status = [
-            'path' => $path,
-            'filename' => $filename,
-            'name' => (string) $request->getData('title'),
-            'size' => File::size($fullPath),
-            'extension' => $extension,
-            'status' => UploadStatus::OK,
-        ];
-
-        $media = MediaController::createDbEntry($status, $request->getHeader()->getAccount());
-        $draw  = DrawImage::fromMedia($media);
-
-        DrawImageMapper::create($draw);
-
-        $response->set('image', $draw->jsonSerialize());
-    }
-
-    /**
-     * Create local image file
-     *
-     * @param string $outputPath Output path
-     * @param string $raw        Base64 encoded image string
-     *
-     * @return bool
-     *
-     * @since  1.0.0
-     */
-    private function createLocalFile(string $outputPath, string $raw) : bool
-    {
-        $imageData = ImageUtils::decodeBase64Image($raw);
-        File::put($outputPath, $imageData);
-
-        return true;
     }
 }
