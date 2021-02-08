1b6f5ef350d5d06664e665bd3fc75829b862af5e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Draw' => [
+    'Created' => '',
+    'Creator' => '',
+    'Draw' => '',
+    'GroupUser' => '',
+    'Images' => '',
+    'Layout' => '',
+    'Name' => '',
+    'Permission' => '',
+    'Start' => '',
+]];
--- Theme/Backend/Lang/en.lang.php
+++ Theme/Backend/Lang/en.lang.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 8.0
  *
- * @package   Modules\Draw
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -13,13 +12,13 @@
 declare(strict_types=1);
 
 return ['Draw' => [
-    'Created'    => 'Created',
-    'Creator'    => 'Creator',
-    'Draw'       => 'Draw',
-    'GroupUser'  => 'Group/User',
-    'Images'     => 'Images',
-    'Layout'     => 'Layout',
-    'Name'       => 'Name',
+    'Created' => 'Created',
+    'Creator' => 'Creator',
+    'Draw' => 'Draw',
+    'GroupUser' => 'Group/User',
+    'Images' => 'Images',
+    'Layout' => 'Layout',
+    'Name' => 'Name',
     'Permission' => 'Permission',
-    'Start'      => 'Start',
+    'Start' => 'Start',
 ]];
