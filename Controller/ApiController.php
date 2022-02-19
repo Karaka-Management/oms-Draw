e389d14d1efa4552ce69882a126a8491c26fb4c9
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-02-19 13:57:38 +0100

    fix name

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Orange Management
+ * Karaka
  *
  * PHP Version 8.0
  *
@@ -9,7 +9,7 @@
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
- * @link      https://orange-management.org
+ * @link      https://karaka.app
  */
 
 declare(strict_types=1);
@@ -33,7 +33,7 @@ use phpOMS\Utils\ImageUtils;
  *
  * @package Modules\Draw
  * @license OMS License 1.0
- * @link    https://orange-management.org
+ * @link    https://karaka.app
  * @since   1.0.0
  */
 final class ApiController extends Controller
