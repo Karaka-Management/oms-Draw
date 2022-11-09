968c4b47c7488de631e650db73c6efbf8a5ce618
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-09 23:02:52 +0100

    reduce serialization of null models

--- Models/NullDrawImage.php
+++ Models/NullDrawImage.php
@@ -35,4 +35,12 @@ final class NullDrawImage extends DrawImage
     {
         $this->id = $id;
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
