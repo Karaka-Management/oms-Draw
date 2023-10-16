7d0f8ee80b62b77906607958f7e9e914a66a07d8
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:39:38 +0000

    Add test cases for null models

--- tests/Models/NullDrawImageTest.php
+++ tests/Models/NullDrawImageTest.php
@@ -23,7 +23,7 @@ final class NullDrawImageTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\Draw\Models\NullDrawImage
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,11 +32,21 @@ final class NullDrawImageTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Draw\Models\NullDrawImage
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
         $null = new NullDrawImage(2);
         self::assertEquals(2, $null->id);
+    }
+
+    /**
+     * @covers Modules\Draw\Models\NullDrawImage
+     * @group module
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullDrawImage(2);
+        self::assertEquals(['id' => 2], $null);
     }
 }
